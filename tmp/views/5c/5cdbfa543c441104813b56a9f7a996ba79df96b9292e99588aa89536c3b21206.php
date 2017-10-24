c90f98146f6dc7e2774be8d9aee4f65683dbc764
Eliam LOTONGA <eliam.lotonga@ives.fr>
2017-10-24 14:35:20 +0200

    begin learning Slim framework

---
+++ tmp/views/5c/5cdbfa543c441104813b56a9f7a996ba79df96b9292e99588aa89536c3b21206.php
@@ -0,0 +1,37 @@
+<?php
+
+/* home.twig */
+class __TwigTemplate_44550b7a0fc580f61f465fdbb4566b142e55c473803b7aee565fb1b5c89281cd extends Twig_Template
+{
+
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<h1>hello word</h1>";
+    }
+
+    public function getTemplateName()
+    {
+        return "home.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "home.twig", "Z:\\djanah\\dev\\elotonga\\test\\SlimFrameWork\\templates\\home.twig");
+    }
+}
